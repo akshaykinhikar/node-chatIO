9bee732992c234956013c36a31e0d4d24849325e
akshaykinhikar <kinhikar.akshay32@gmail.com>
2020-04-08 18:37:38 +0530

    connection established and disconnected

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 const PORT = 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-})
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+});
 
 http.listen(PORT, function(){
     console.log('server listening on '+PORT);
-})
+});
